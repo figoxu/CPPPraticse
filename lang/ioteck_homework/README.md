4e93491d11fe85303e77c9940a412c4231f743e8
jianhui xu <jianhui.xu@tendcloud.com>
2016-08-01 18:52:36 +0800

    ioteck homework sample

---
+++ lang/ioteck_homework/README.md
@@ -0,0 +1,2 @@
+# iotek_cpp_course_homework
+上海海同科技海同网校C++课程课后作业.
---
+++ lang/ioteck_homework/第一章 C++简介/简单/源码/hello.cpp
@@ -0,0 +1,13 @@
+//C++使用双斜杠进行单行注释
+/*
+ * C++ Hello world!
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello cpp, welcome to Shanghai IOTEK!" << endl;
+	return 0;
+}
