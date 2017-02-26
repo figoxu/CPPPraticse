a6a45ef9fe37a130bd66a305df8bd835a9823dbc
jianhui xu <jianhui.xu@tendcloud.com>
2017-02-26 14:35:54 +0800

    boost : scoped array ptr

--- boost/ptr/main.cpp
+++ boost/ptr/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <boost/scoped_ptr.hpp>
+#include <boost/scoped_array.hpp>
 
 using namespace std;
 
@@ -10,8 +11,16 @@ void scopedPtr(){
     i.reset(new int);
 }
 
+void scopedPtr_array(){
+    boost::scoped_array<int> i(new int[2]);
+    *i.get() = 1;
+    i[1] = 2;
+    i.reset(new int[3]);
+}
+
 int main() {
     scopedPtr();
+    scopedPtr_array();
     cout << "Hello, World!" << endl;
     return 0;
 }
